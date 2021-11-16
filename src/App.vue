635a69cc818e5ef117eb76b15de00576b314dd5f
yujin <rosepark306@gmail.com>
2021-11-16 12:19:44 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-      <router-link class="navbar-brand nav-link" aria-current="page" to="./main">Blog</router-link>
+      <router-link class="navbar-brand nav-link" aria-current="page" to="/main">Blog</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="./main">Home</router-link>
+          <router-link class="nav-link active" aria-current="page" to="/main">Home</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="./list">List</router-link>
+          <router-link class="nav-link" to="/list">List</router-link>
         </li>
       </ul>
     </div>
